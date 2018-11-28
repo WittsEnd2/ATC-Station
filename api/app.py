8f2fb726821f97d169b41ed029c9a209ab6aa552
= <michael.r1797@gmail.com>
2018-11-27 20:22:56 -0500

    Added periodic loading of json data to endpoint /api/get_flights

--- api/app.py
+++ api/app.py
@@ -1,13 +1,37 @@
 from flask import Flask
+import json
+import os
+import time, threading
 
 app = Flask(__name__)
-@app.route('/')
+
+data = []
+DATAFOLDER = "data/"
+
+
+def update_json():
+    files = os.listdir(DATAFOLDER)
+    for name in files:
+        if "history" in name:
+            f = open(DATAFOLDER + "" + name, "r")
+            x = f.read()
+            f.close()
+
+            if x not in data:
+                data.append(json.loads(x))
+    threading.Timer(10, update_json).start()
+
+
+@app.route("/")
 def index():
     return "Invalid API endpoint"
 
-@app.route('/api/get_flights')
-def get_flights():
-    return "{'key':'value'}"
 
-if __name__ == '__main__':
+@app.route("/api/get_flights")
+def get_flights():
+    return json.dumps(data)
+
+
+if __name__ == "__main__":
+    update_json()
     app.run(port=1337)
