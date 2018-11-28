f3adda203a52b5e7fb19f816687c73e936dfcbe3
= <michael.r1797@gmail.com>
2018-11-27 21:31:35 -0500

    added some comments to complete

--- api/app.py
+++ api/app.py
@@ -2,22 +2,24 @@ from flask import Flask
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
+import tweepy
 import json
 import os
 import time, threading
-import tweepy
 import emoji
 import unicodedata
 import datetime
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-nltk.download('punkt')
+
+nltk.download("punkt")
 
 consumer_key = "kEaMOwaPFjdwXelB8rwMXh1Yg"
 consumer_secret = "2AKsg0BwxI1bDErlQDUgmNVDvOwzx98htaiYKGXeZzoHv71Jy6"
 
 access_token = "839692039-VulkCGE4QZRZlKYQtDuNjRWgJnxsCdeSfqaPuObs"
 access_token_secret = "08AltS6hMTj5Y7sDh2cSLZCrtuqfEYQJ5LTqEI2N4FLN0"
+
 
 def getSentiment(tweet):
     sid = SentimentIntensityAnalyzer()
@@ -29,58 +31,54 @@ def getSentiment(tweet):
     for sentence in sentList:
         count += 1
         ss = sid.polarity_scores(sentence)
-        sent += ss['compound']  # Tally up the overall sentiment
+        sent += ss["compound"]  # Tally up the overall sentiment
 
     if count != 0:
         sent = float(sent / count)
-        
+
     return sent
 
 
 class listener(StreamListener):
-	def __init__(self):
-		super().__init__()
-		self.counter = 0 
-	# def on_status(self, status):
-	# 	print(status)
-	# def on_data(self, data):
-	# 	print(data)
-	def on_data(self, data):
-		
-		data = str(emoji.demojize(data))
-		
-		decoded = json.loads(str(data))
-		# if 'place' in decoded and decoded['place'] is not None:
-			# loc = decoded['place']['bounding_box']['coordinates'][0][0]
-			
-		tweet = str(emoji.demojize(decoded['text']).encode("unicode_escape"))
-		tweet = tweet[1:]
-		tweet = tweet.strip("\n")
-		tweet = tweet.strip("\.")
+    def __init__(self):
+        # super().__init__()
+        self.counter = 0
+        # def on_status(self, status):
+        # 	print(status)
+        # def on_data(self, data):
+        # 	print(data)
 
-		tweet = tweet.replace("\n",". ")
-		tweet = tweet.replace("\\'","'")
-		tweet = tweet.replace("\\","")
-		tweet = tweet.replace("\\\.",".")
-		tweet = tweet.replace("\"", "'")
-		tweet = tweet.replace("\\n",". ")
-		tweet = tweet.replace ("\\,", "")
-		tweet = tweet.replace(",", "")
-		print(tweet)
-		sid = SentimentIntensityAnalyzer()
-		print(getSentiment(tweet))
-		print()
-	
+    def on_data(self, data):
 
-		sendJson = '{ "' + str(datetime.datetime.now()) + '": "' + tweet + '" }'
+        data = str(emoji.demojize(data))
 
-		
-	# 		tweetLower = tweet.lower()
-	# 		if(company in tweetLower or ticker_symbol in tweetLower):
-	# 			companyTweets.write('{"tweet": "' + tweet +'", "coordinates": ' + str(loc) + '}\n')
-	# 			companyTweets.flush()
-	def on_error(self, status):
-		print (status)
+        decoded = json.loads(str(data))
+        # if 'place' in decoded and decoded['place'] is not None:
+        # loc = decoded['place']['bounding_box']['coordinates'][0][0]
+
+        tweet = str(emoji.demojize(decoded["text"]).encode("unicode_escape"))
+        tweet = tweet[1:]
+        tweet = tweet.strip("\n")
+        tweet = tweet.strip("\.")
+
+        tweet = tweet.replace("\n", ". ")
+        tweet = tweet.replace("\\'", "'")
+        tweet = tweet.replace("\\", "")
+        tweet = tweet.replace("\\\.", ".")
+        tweet = tweet.replace('"', "'")
+        tweet = tweet.replace("\\n", ". ")
+        tweet = tweet.replace("\\,", "")
+        tweet = tweet.replace(",", "")
+        print(tweet)
+        sid = SentimentIntensityAnalyzer()
+        print(getSentiment(tweet))
+        print()
+
+        sendJson = '{ "' + str(datetime.datetime.now()) + '": "' + tweet + '" }'
+
+    def on_error(self, status):
+        print(status)
+
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -88,8 +86,8 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 myStreamListener = listener()
-myStream = Stream(auth = api.auth, listener=myStreamListener)
-
+myStream = Stream(auth=api.auth, listener=myStreamListener)
+myStream.filter(languages=["en"], track=["United Airlines", "airlines", "#ASA1958"])
 
 
 app = Flask(__name__)
@@ -107,6 +105,12 @@ def update_json():
             f.close()
             y = json.loads(x)
             if y not in data:
+                # 1. Get flight name
+                # 2. Lookup keywords to get from Twitter
+                # 3. Create a new Tweet stream with a filter for those tweet keywords
+                # 4. Then get the data every 10 seconds using the threading below
+                # 5. Pass to sentiment
+                # 6. Serve json dumps
                 data.append(y)
 
     threading.Timer(10, update_json).start()
@@ -119,7 +123,6 @@ def index():
 
 @app.route("/api/get_flights")
 def get_flights():
-
     return json.dumps(data)
 
 
