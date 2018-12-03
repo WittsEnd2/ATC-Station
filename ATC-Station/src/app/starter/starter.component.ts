f32abd2a32d0f0f6fd5822feee1ef9e49a8c53ff
Michael Wittner <mwittner2@gmail.com>
2018-12-03 17:08:14 -0500

    Http queries in angular

--- ATC-Station/src/app/starter/starter.component.ts
+++ ATC-Station/src/app/starter/starter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-starter',
@@ -7,21 +8,30 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class StarterComponent implements OnInit, OnDestroy {
 
+  private flightData; 
   bodyClasses = 'skin-black sidebar-mini';
   body: HTMLBodyElement = document.getElementsByTagName('body')[0];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
 
   ngOnInit() {
     // add the the body classes
     this.body.classList.add('skin-black');
     this.body.classList.add('sidebar-mini');
+    this.getFlightData()
   }
 
   ngOnDestroy() {
     // remove the the body classes
     this.body.classList.remove('skin-black');
     this.body.classList.remove('sidebar-mini');
+  }
+  getFlightData(){ 
+    this.http.get("https://localhost:5000/api/get_flights").subscribe(data => {
+      this.flightData = data;
+      console.log(data);
+    })
   }
 
 }
